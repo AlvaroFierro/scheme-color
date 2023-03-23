40dbaffc31663b4882feb4c7feaa38449e00825a
Alvaro Fierro <business.afierro@gmail.com>
2023-03-22 18:04:00 -0600

    Feat:get data from thecolor api and styles

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-import './style.css'
-
+const url = 'https://www.thecolorapi.com/scheme'
 const colorPicker = document.getElementById('color-picker')
+const schemeSelector = document.getElementById('scheme-selector')
+const getSchemeBtn = document.getElementById('get-scheme-btn')
+const paletteEl = document.getElementById('palette-container')
+let schemes = ''
 
-const url = 'https://www.thecolorapi.com/'
-
-let color = '#000000'.replace(/0/g, function () {
+let hexColor = '#000000'.replace(/0/g, function () {
   return (~~(Math.random() * 16)).toString(16)
 })
 
-colorPicker.value = color
+colorPicker.value = hexColor
+const color = hexColor.slice(1)
+
+fetchColors()
+getSchemeBtn.addEventListener('click', () => {
+  fetchColors()
+})
+
+function fetchColors() {
+  fetch(`${url}?hex=${color}&${schemeSelector.value}`)
+    .then((res) => res.json())
+    .then((data) => {
+      getSchemeColor(data.colors)
+      if (!schemes) {
+        schemes = data._links.schemes
+        optionSchemes()
+      }
+    })
+}
+
+function optionSchemes() {
+  Object.entries(schemes).forEach(([key, value]) => {
+    schemeSelector.add(new Option(key.charAt(0).toUpperCase() + key.slice(1), value))
+  })
+}
+
+function getSchemeColor(colors) {
+  paletteEl.innerHTML = colors
+    .map((color) => {
+      return `
+        <div style="background-color:${color.hex.value}">
+          <div class="color-details">
+            <div class="color-hex">${color.hex.clean}</div>
+            <div class="name-color">${color.name.value}</div>
+          </div>
+        </div>
+        `
+    })
+    .join('')
+}
